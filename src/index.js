61b5b556ea8e4d5ed4804057cc30aebee3accde3
mlorenzm <mlorenzomohamed@gmail.com>
2023-08-27 18:51:28 +0100

    Add functions for weather API calling and refining

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-console.log("yeeha");
 import "./style.scss";
+import { getWeather } from "./modules/appModel.js";
+
+getWeather("Los Angeles");
+
+// https://api.openweathermap.org/data/2.5/weather?q=London&lang=sp&appid=c7cb5cbe6852930a001d50dd8e3d50b1
---
+++ src/modules/appModel.js
@@ -0,0 +1,26 @@
+const apiKey = "c7cb5cbe6852930a001d50dd8e3d50b1";
+
+async function getWeather(city) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=en&appid=${apiKey}&units=metric`,
+    { mode: "cors" }
+  );
+  const data = await response.json();
+  console.log(data);
+  console.log(processData(data));
+}
+
+function processData(data) {
+  const city = data.name;
+  const country = data.sys.country;
+  const temperature = data.main.temp;
+  const weather = data.weather[0].main;
+  return {
+    city: city,
+    country: country,
+    temperature: temperature,
+    weather: weather,
+  };
+}
+
+export { getWeather };
