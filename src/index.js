72a5e96c37ea3427f58a17e341adf46227f33860
mlorenzm <mlorenzomohamed@gmail.com>
2023-08-27 19:18:29 +0100

    broken?

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import "./style.scss";
 import { getWeather } from "./modules/appModel.js";
-
-getWeather("Los Angeles");
-
+import { changeWeather } from "./modules/appController.js";
 // https://api.openweathermap.org/data/2.5/weather?q=London&lang=sp&appid=c7cb5cbe6852930a001d50dd8e3d50b1
---
+++ src/modules/appController.js
@@ -0,0 +1,13 @@
+import { getWeather } from "./appModel.js";
+const form = document.getElementById("form");
+console.log(form);
+
+function changeWeather() {
+  const formBox = document.getElementById("textbox");
+  // form.addEventListener("submit", () => {
+  //   event.preventDefault();
+  //   console.log("form");
+  // });
+}
+
+export { changeWeather };
