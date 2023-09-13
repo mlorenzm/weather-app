83b8382a6605aaf9e3e2ad34b43834bb060cfbb3
mlorenzm <mlorenzomohamed@gmail.com>
2023-09-13 21:02:21 +0100

    changed language to SP

--- src/modules/appModel.js
+++ src/modules/appModel.js
@@ -2,7 +2,7 @@ const apiKey = "c7cb5cbe6852930a001d50dd8e3d50b1";
 
 async function fetchWeather(city) {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=en&appid=${apiKey}&units=metric`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=es&appid=${apiKey}&units=metric`,
     { mode: "cors" }
   );
   if (response.status !== 200) {
@@ -10,11 +10,12 @@ async function fetchWeather(city) {
     //console.log("404");
   } else {
     const data = await response.json();
+    console.log(data);
     const obj = {
       cityName: data.name,
       country: data.sys.country,
       temperature: data.main.temp,
-      weather: data.weather[0].main,
+      weather: data.weather[0].description,
     };
     return obj;
   }
@@ -23,6 +24,7 @@ async function fetchWeather(city) {
 async function getWeather(city) {
   const res = await fetchWeather(city);
   console.log(res);
+  return res;
 }
 
 export { getWeather };
