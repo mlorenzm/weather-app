32be53c133035b3bdf1b0c9d14c3538cc96662fd
mlorenzm <mlorenzomohamed@gmail.com>
2023-10-01 12:52:14 +0100

    minor styling

--- src/modules/appView.js
+++ src/modules/appView.js
@@ -3,9 +3,10 @@ import { getWeather } from "./appModel";
 function updateWeatherView(data) {
   const main = document.getElementById("main");
 
-  const text = `In ${data.cityName}, ${data.country}, there are ${data.temperature} ºC, and it's ${data.weather}`;
+  const text = `In <span id='city-name'>${data.cityName}, ${data.country}</span> there are ${data.temperature}ºC, and it's <u>${data.weather}</u>`;
   const div = document.createElement("div");
-  div.textContent = text;
+  div.innerHTML = text;
+  div.setAttribute("id", "textID");
   main.removeChild(main.lastChild);
   main.appendChild(div);
 }
